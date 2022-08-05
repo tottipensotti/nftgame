e3be534ee9f9ec3471857700644818f6c710b469
tottipensotti <ag.pensotti@gmaile.com>
2022-08-04 21:23:29 -0300

    Added characters data

--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,12 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('TheGame');
     const gameContract = await gameContractFactory.deploy(
-        [], // names
-        [], // images
-        [], // health
-        [], // attack pts
-        [] // defense pts
-
-
-
+        ['Batman', 'Spiderman' , 'Thor', 'Ironman'], // names
+        ['https://imgur.com/6u2u5Zb', 'https://imgur.com/5rxXE1h',
+            'https://imgur.com/TAz2BnA', 'https://imgur.com/h9pgPOP'], // images
+        [400, 250, 600, 450], // health
+        [300, 200, 600, 550 ], // attack pts
+        [300, 200, 400, 350] // defense pts
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
