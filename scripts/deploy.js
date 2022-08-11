5f18144ef1d884a18b55d2f3994f494d8b11cf1d
tottipensotti <ag.pensotti@gmail.com>
2022-08-10 21:12:41 -0300

    commit to main

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,9 +17,21 @@ const main = async () => {
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
     
+    let txn;
+    txn = await gameContract.mintCharacterNFT(3);
+    await txn.wait();
+    
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    console.log("Done.");
+    
     // TokenURI is a function inherited from ERC721 that returns the data attached to the NFT
-    let returnedTokenUri = await gameContract.tokenURI(1);
-    console.log("Token URI:", returnedTokenUri);
+    //let returnedTokenUri = await gameContract.tokenURI(1);
+    //console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
--- scripts/run.js
+++ scripts/run.js
@@ -13,23 +13,25 @@ const main = async () => {
         60,
         40
     );
+    
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
-
-    // mint the NFT!
+    
     let txn;
     txn = await gameContract.mintCharacterNFT(3);
     await txn.wait();
-
+    
     txn = await gameContract.attackBoss();
     await txn.wait();
 
     txn = await gameContract.attackBoss();
     await txn.wait();
+
+    console.log("Done.");
     
     // TokenURI is a function inherited from ERC721 that returns the data attached to the NFT
-    let returnedTokenUri = await gameContract.tokenURI(1);
-    console.log("Token URI:", returnedTokenUri);
+    //let returnedTokenUri = await gameContract.tokenURI(1);
+    //console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
